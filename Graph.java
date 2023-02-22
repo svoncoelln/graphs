07cf10c09009747a84325280ce3410ffbb706557
von Coelln <svoncoelln@friendsbalt.org>
2023-02-22 10:15:19 -0500

    add adjacent method

--- Graph.java
+++ Graph.java
@@ -1,5 +1,6 @@
 import edu.princeton.cs.algs4.Bag;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Graph {
@@ -12,18 +13,29 @@ public class Graph {
         g = (Bag<Integer>[]) new Bag[v];
     }
 
+    public Graph(InputStream in) {
+        Scanner scan = new Scanner(in);
+        V = scan.nextInt();
+        E = scan.nextInt();
+        while (scan.hasNext()) {
+            addEdge(scan.nextInt(), scan.nextInt());
+        }
+    }
+
     public void addEdge(int v, int w) {
         g[v].add(w);
         g[w].add(v);
         E++;
     }
 
-    public Graph(InputStream in) {
-        Scanner scan = new Scanner(in);
-        V = scan.nextInt();
-        E = scan.nextInt();
-        for (int i = 0; i < E; i++) {
-            addEdge(scan.nextInt(), scan.nextInt());
+    public Iterable<Integer> adj(int v) {
+        return g[v];
+    }
+    public static void main(String[] args) {
+        Graph g = new Graph(System.in);
+        ArrayList<Integer> seven = (ArrayList<Integer>) g.adj(7);
+        for (int i : seven) {
+            System.out.println(i);
         }
     }
 }
